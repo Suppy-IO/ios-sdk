e7d2b4b01448a012ccf61df411fbf7835044a098
Ricardo Rautalahti-Hazan <ricardo@suppy.io>
2020-09-18 12:32:47 +0300

    delete integration test with real client

--- Tests/RetryTests.swift
+++ Tests/RetryTests.swift
@@ -58,32 +58,4 @@ class RetryTests: XCTestCase {
 
         XCTAssertEqual(mockFetcher.numberOfCalls, expects)
     }
-
-    func testRealTest() {
-
-        let dependencies = [Dependency(name: "Privacy Policy", value: URL(string: "https://default-local-url.com")!, mappedType: .url),
-                            Dependency(name: "Application Title", value: "Initial App Title", mappedType: .string),
-                            Dependency(name: "Recommended Version", value: "1.0.0", mappedType: .string),
-                            Dependency(name: "Acceptance Ratio", value: 1.61803, mappedType: .number),
-                            Dependency(name: "Number of Seats", value: 2, mappedType: .number),
-                            Dependency(name: "Background Color", value: "white", mappedType: .string),
-                            Dependency(name: "Product List", value: [], mappedType: .array)]
-
-        let suppy = SuppyConfig(configId: "5f43879d25bc1e682f988129",
-                                applicationName: "Swift Example",
-                                dependencies: dependencies,
-                                enableDebugMode: true)
-
-        let completionExpectation = expectation(description: "completion should get called")
-
-        suppy.fetchConfiguration {
-            completionExpectation.fulfill()
-        }
-
-        waitForExpectations(timeout: 1) { error in
-            if let error = error {
-                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
-            }
-        }
-    }
 }
